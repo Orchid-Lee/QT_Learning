dd49e0697f95875d7ea56976af573c2b4416ba11
zhangxinjie <www.zhangxinjie@gmail.com>
2024-10-30 16:14:27 +0800

    feat: c++时间类

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -10,45 +11,15 @@ public:
     double length;
     double width;
     double height;
-    double calc(Box *box);
-    Box();                  
-    Box(double len, double wid, double hei);
-    ~Box();
-
 private:
     int id;
-};
-
-Box::Box()
-{
-    cout << "Box init()" << endl;
-};
-
-Box::~Box()
-{
-    cout << "obj del." << endl;
-};
-
-Box::Box(double len, double wid, double hei):length(len), width(wid), height(hei)
-{
-};
-
-double Box::calc(Box *box)
-{
-    return box->length * box->height * box->width;
-};
-
-class House: public Box
-{
 };
 
 
 int main()
 {
-    House house;
-    house.length = 12;
-    house.width = 12;
-    house.height = 1;
-    cout << "calc:" << house.calc(&house) << endl;
+    time_t now = time(0);
+    char* str = ctime(&now);
+    cout << "T:" << str << endl;
     return 0;
 }
