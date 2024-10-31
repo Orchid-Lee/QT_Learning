4f17f9ea6f2d4b2a733d100a8c6f6ac9a9ae16db
zhangxinjie <www.zhangxinjie@gmail.com>
2024-10-31 15:16:44 +0800

    feat: c++引用

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
-#include <csignal>
-#include <unistd.h>
 
 using namespace std;
 
-void sig_handler(int sig_num)
-{
-    cout << "recv sig: " << sig_num << endl;
-    exit(sig_num);
-}
-
-int main()
-{
-    int i = 0;
-    signal(SIGINT, sig_handler);
-    while (++i)
+int main() {
+    int a[] = {1, 3, 5, 7};
+    int (&b)[4] = a;
+    cout << "b[]:" << b << endl;
+    for (size_t i = 0; i < 4; i++)
     {
-        if (i == 3)
-        {
-            raise(SIGINT);
-        }
-        cout << "Going to sleep." << endl;
-        sleep(1);
+        cout << "b[" << i << "] = " << b[i] << endl;
     }
+    
     return 0;
 }
