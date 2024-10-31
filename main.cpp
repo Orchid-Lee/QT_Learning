c1637a85a146e5477c8256a2b5bf0d7855cc138d
zhangxinjie <www.zhangxinjie@gmail.com>
2024-10-31 09:16:29 +0800

    feat: 文件输入输出

--- main.cpp
+++ main.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-class Box
-{
-protected:
-    double len;
-    double wid;
-    double hei;
-
-public:
-    virtual double area() = 0;
-};
-
-class Rect: public Box
-{
-public:
-    double area()
-    {
-        cout << "sub area." << endl;
-        return 0;
-    };
-};
-
 int main()
 {
-    Box *box;
-    Rect rect;
-    box = &rect;
-    cout << "rect:" << box->area() << endl;
+    char data[100];
+    ofstream ofstream;
+    ofstream.open("/home/moon/project_folder/c_plus/test.txt", ios::out | ios::trunc);
+    cout << "write to the file" << endl;
+    cout << "Enter your name:";
+    cin.getline(data, 100);
+    ofstream << data << endl;
+    ofstream.close();
+
+    ifstream ifstream;
+    ifstream.open("/home/moon/project_folder/c_plus/test.txt");
+    cout << "read the file." << endl;
+    ifstream >> data;
+    ifstream.close();
+    cout << "data:" << data << endl;
     return 0;
 }
