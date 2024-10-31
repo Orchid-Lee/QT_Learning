26853f8c103c1bf3e68dd1ee2c6559b28c695aee
zhangxinjie <www.zhangxinjie@gmail.com>
2024-10-31 15:32:35 +0800

    feat: c++线程基础 线程创建

--- main.cpp
+++ main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
-int main() {
-    int a[] = {1, 3, 5, 7};
-    int (&b)[4] = a;
-    cout << "b[]:" << b << endl;
-    for (size_t i = 0; i < 4; i++)
+void printMsg(int count)
+{
+    for (size_t i = 0; i < count; i++)
     {
-        cout << "b[" << i << "] = " << b[i] << endl;
+        cout << "Hello from thread (function pointer)!" << endl;
     }
-    
+}
+
+class PrintMsg
+{
+public:
+    void operator()(int count) const
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            cout << "Hello from thread (function obj)!" << endl;
+        }
+    }
+};
+
+int main()
+{
+    thread t1(printMsg, 5);
+    t1.join();
+
+    thread t2(PrintMsg(), 5);
+    t2.join();
+
+    thread t3([](int count)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            cout << "Hello from thread (function 3)!" << endl;
+        }
+    }, 5);
+    t3.detach();
     return 0;
 }
