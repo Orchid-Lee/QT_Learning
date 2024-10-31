7cd3a8217b175ccd56828deeb89c17b45649600a
zhangxinjie <www.zhangxinjie@gmail.com>
2024-10-31 09:37:47 +0800

    feat: c++异常处理

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
+class MyE : public exception
+{
+    const char* what() const throw()
+    {
+        return "C++ exception";
+    }
+};
+
 int main()
 {
-    char data[100];
-    ofstream ofstream;
-    ofstream.open("/home/moon/project_folder/c_plus/test.txt", ios::out | ios::trunc);
-    cout << "write to the file" << endl;
-    cout << "Enter your name:";
-    cin.getline(data, 100);
-    ofstream << data << endl;
-    ofstream.close();
-
-    ifstream ifstream;
-    ifstream.open("/home/moon/project_folder/c_plus/test.txt");
-    cout << "read the file." << endl;
-    ifstream >> data;
-    ifstream.close();
-    cout << "data:" << data << endl;
+    throw MyE();
     return 0;
 }
